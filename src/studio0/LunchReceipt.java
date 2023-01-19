b3d7722c63e8db5ce6271d99528bd827cf55e744
rin.iimi.2022 <rin.iimi.2022@rins-mbp.wusm-wifi.wucon.wustl.edu>
2023-01-19 12:03:52 -0600

    finished up to messages

--- src/studio0/LunchReceipt.java
+++ src/studio0/LunchReceipt.java
@@ -11,7 +11,7 @@ public class LunchReceipt {
 		
 		double subtotal = burritoPrice + drinkPrice + chipsPrice + guacPrice;
 		double taxRate = 0.04225;
-		double taxAmount = subtotal * taxRate;
+		double taxAmount = subtotal * (1 + taxRate);
 		double total = subtotal + taxAmount;
 		
 		System.out.println("Your total was: $" + total);
